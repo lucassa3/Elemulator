902064e58b646618209f1787d45f1e2d5e9f296b
khalilyassine <khalilyyassine@hotmail.com>
2016-11-18 11:05:05 -0200

    Clock completo

--- src/br/edu/insper/elemulator/controller/Clock.java
+++ src/br/edu/insper/elemulator/controller/Clock.java
@@ -1,27 +1,28 @@
 package br.edu.insper.elemulator.controller;
 
 import java.util.Timer;
-import java.util.TimerTask;
+
 
 
 public class Clock {
-/*	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException {
 	Timer tempo;
 	int[] k = new int[]{0,1,1,0,1,1,0,1,0,1};
-for (int i = 0; i<10; i++){
-	System.out.println(k[i]);
-	Thread.sleep(1000);
+	for (int i = 0; i<10; i++){
+		int x = i;
+	 	tempo = new Timer();
+		tempo.schedule(new Tempo() {
+	    	@Override
 
-	
-	}}}
 
-/*tempo = new Timer();
-tempo.schedule(new TimerTask() {
-    @Override
-    public void run() {
+		    public void run() {
 
-    	    System.out.println("banana");
+	    	    System.out.println(x);
 
-    }
-}, 750);
-*/}
+		    }
+		}, 750);
+	}
+}
+}
+
+
---
+++ src/br/edu/insper/elemulator/controller/Tempo.java
@@ -0,0 +1,17 @@
+package br.edu.insper.elemulator.controller;
+import java.util.TimerTask;
+public class Tempo extends TimerTask{
+	private int i;
+	public Tempo(){
+		
+	i=0;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+}
